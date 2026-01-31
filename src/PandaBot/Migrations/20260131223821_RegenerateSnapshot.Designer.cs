932df68cc79292d06365a50c25f198a3660b6170
homotechsual <mikey@homotechsual.dev>
2026-01-31 22:38:58 +0000

    feat: Regenerate database snapshot and update version to 1.0.1

--- src/PandaBot/Migrations/20260131223821_RegenerateSnapshot.Designer.cs
+++ src/PandaBot/Migrations/20260131223821_RegenerateSnapshot.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PandaBot.Core.Data;
 
@@ -10,9 +11,11 @@ using PandaBot.Core.Data;
 namespace PandaBot.Migrations
 {
     [DbContext(typeof(PandaBotContext))]
-    partial class PandaBotContextModelSnapshot : ModelSnapshot
+    [Migration("20260131223821_RegenerateSnapshot")]
+    partial class RegenerateSnapshot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.0");
